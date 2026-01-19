dd454790ee24d0225ee232c1d047307cf4f56a27
Hakim <helkabouss@akonis.fr>
2026-01-19 13:43:12 +0100

    update

--- Anozrway/anozrway_modules/__init__.py
+++ Anozrway/anozrway_modules/__init__.py
@@ -1,1 +1,6 @@
-# package
+from sekoia_automation.module import Module
+
+
+class AnozrwayModule(Module):
+    name = "Anozrway"
+    description = "Anozrway provides domain-based breach and leak intelligence through a secured API."
--- Anozrway/main.py
+++ Anozrway/main.py
@@ -1,18 +1,7 @@
-from sekoia_automation.module import Module
-
+from anozrway_modules import AnozrwayModule
 from anozrway_modules.domain_search import DomainSearch
-
-
-class AnozrwayModule(Module):
-    name = "Anozrway"
-    description = "Anozrway provides domain-based breach and leak intelligence through a secured API."
-
-    # Register actions by their docker_parameters string
-    actions = {
-        "domain_search": DomainSearch,
-    }
-
 
 if __name__ == "__main__":
     module = AnozrwayModule()
+    module.register(DomainSearch, "domain_search")
     module.run()
