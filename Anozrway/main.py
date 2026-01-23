21623b4df8daa4d6776766670ae794d657889926
Hakim <helkabouss@akonis.fr>
2026-01-23 10:07:53 +0100

    Delete Domain Search Action

--- Anozrway/main.py
+++ Anozrway/main.py
@@ -1,13 +1,9 @@
 from sekoia_automation.module import Module
 
-from anozrway_modules.domain_search import DomainSearch
 from anozrway_modules.historical_connector import AnozrwayHistoricalConnector
 
 if __name__ == "__main__":
     module = Module()
-
-    # Action (playbook)
-    module.register(DomainSearch, "domain_search")
 
     # Connector (intake)
     module.register(AnozrwayHistoricalConnector, "anozrway_historical")
